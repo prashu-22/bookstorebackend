825553f1cb0d7efc11166fb9d24c5c10bc0db943
prashu-22 <139197155+prashu-22@users.noreply.github.com>
2024-05-20 22:01:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
+import {mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
