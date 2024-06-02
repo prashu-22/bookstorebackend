a58eb45b0d463ec75acac6abe867f9b3a249bce4
prashu-22 <139197155+prashu-22@users.noreply.github.com>
2024-06-02 22:57:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export let PORT = process.env.PORT || 4000;;
 
-export const mongoDBURL =
-  'mongodb+srv://Prashanth:B9LkMhK4hd7vmBSi@bookstoreyt.cyirbrw.mongodb.net/?retryWrites=true&w=majority&appName=bookstoreYT';
+export const mongoDBURL ='mongodb://localhost:27017/'
+  // 'mongodb+srv://Prashanth:B9LkMhK4hd7vmBSi@bookstoreyt.cyirbrw.mongodb.net/?retryWrites=true&w=majority&appName=bookstoreYT';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
